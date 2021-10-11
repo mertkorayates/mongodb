1abfbab0c58f18370408c2b7aab851bde6b1edee
mertkorayates <mertkorayates200@gmail.com>
2021-10-11 20:25:58 +0300

    Update main.js

--- nodejsproje/main.js
+++ nodejsproje/main.js
@@ -3,8 +3,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
-var User = require("./mongodb-test/schema.js")
-
+var User = require("./mongodb-test/schema.js");
+app.use(bodyParser.json());
+var id;
 mongoose.connect("mongodb://127.0.0.1:27017/nodejsproje?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",(err)=>{
     if(!err){
         console.log("connected")
@@ -55,12 +56,44 @@ app.get("/ekle/:isim",(req,res)=>{
 
 
 
+app.get("/phoneall",(req,res)=>{
+    User.find({},(err,data)=>{
+    if(err){
+        console.log("PHONE ALL ERR")
+    }else{
+        res.send(data);
+    }
+})
 
-app.post("/espekle",(req,res,next)=>{
+})
+
+
+app.post("/espekle",(req,res)=>{
     try{
-        kontrolEkleESP(req,res).then((data)=>{
-            next();
-        });
+        console.log(req.body.mac)
+         User.find({mac: req.body.mac }).then((result)=>{
+            console.log(result)
+             id = result[0].id;
+            //result[0].adc.push(req.body.adc);  
+            adcSave(req,res);                                
+        
+            }).catch((err)=>{
+                var user1 = new User({
+                    mac : req.body.mac,
+                    adc : [req.body.adc],
+                })
+                user1.save((err)=>{
+                    if(err){
+                        console.log("Kullanıcı Eklenmedi")
+                        res.sendStatus(404);
+                    }else{
+                        console.log("Kullanıcı Eklendi")
+                        res.sendStatus(200);
+                    }
+                })
+            })
+
+        
             
       
 
@@ -68,22 +101,31 @@ app.post("/espekle",(req,res,next)=>{
         console.log("ERR2");
         res.sendStatus(404)
     }
+
+
     
-})
+});
 
 
 
-
-
-
-async function kontrolEkleESP(req,res) {
-   await User.find({mac: req.body.mac }).then((result)=>{
-    console.log(result[0].mac)
-                                                        
-
-    }).catch((err)=>{
-
+function adcSave(req,res) {
+    User.findById(id,(err,data)=>{
+        if(err){
+            console.log("findById ERROR")
+            res.sendStatus(404)
+        }else{
+           data.adc.push(req.body.adc)
+    
+            data.save((err)=>{
+                if(!err){
+                    console.log("Update ERR")
+                    res.sendStatus(404)
+                }
+            })
+            
+        }
     })
 }
+
 
 app.listen(3000);
