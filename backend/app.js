cd3d05bea4b61957f1220d5b24384e25ada2bb0c
mertkorayates <mertkorayates200@gmail.com>
2022-01-27 11:34:56 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -69,9 +69,9 @@ app.post("/login",(req,res,next)=>{
     try{
       Kullanici.find({eposta:req.body.eposta},(err,kullanicilar)=>{
         if (err){
-          throw err;
           res.sendStatus(400)
           next();
+          throw err;
         }
         if(kullanicilar != null){
           if(kullanicilar[0]["eposta"] == req.body.eposta && kullanicilar[0]["sifre"] == req.body.sifre ){
