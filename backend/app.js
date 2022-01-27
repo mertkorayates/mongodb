d9c749cd8905470925e5730f62234f25d0a55d53
mertkorayates <mertkorayates200@gmail.com>
2022-01-27 11:31:53 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.post('/register', (req, res,next) => {
             res.sendStatus(400)
             next();
         } else{
-            Kullanici.create({ kullaniciAdi: req.body.kullaniciAdi, eposta: req.body.eposta, numara:req.body.numara, sifre: req.body.sifre }, (err, result) => {
+            Kullanici.create({ kullaniciAd: req.body.kullaniciAd, eposta: req.body.eposta, numara:req.body.numara, sifre: req.body.sifre }, (err, result) => {
                 if (err) throw err;
                 console.log(result);
                 res.sendStatus(200)
@@ -73,7 +73,7 @@ app.post("/login",(req,res,next)=>{
             res.send(kullanicilar)
             next();
         }else{
-          res.sendStatus(404)
+          res.sendStatus(400)
           next();
         }
       })
