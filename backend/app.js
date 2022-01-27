e8f63fe8b88a33f86de21cbdf264866f8bc80c06
mertkorayates <mertkorayates200@gmail.com>
2022-01-27 16:20:13 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -69,16 +69,16 @@ app.post("/login",(req,res,next)=>{
     try{
       Kullanici.find({eposta:req.body.eposta},(err,kullanicilar)=>{
         if (err) throw err;
-        if(kullanicilar[0] != null){
+ 
           if(kullanicilar[0]["eposta"] == req.body.eposta && kullanicilar[0]["sifre"] == req.body.sifre ){
             res.send(kullanicilar)
             next();
         }
-        }
+        
     
         else{
           res.send("kayitli degil")
-          //next();
+          next();
         }
       })
 
