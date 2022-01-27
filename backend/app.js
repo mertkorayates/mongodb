2a4176a7c16968d2dd70572866f1239a0679491f
mertkorayates <mertkorayates200@gmail.com>
2022-01-27 17:13:38 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -71,15 +71,9 @@ app.post("/login",(req,res,next)=>{
         if (err) throw err;
          
           if(kullanicilar[0]){
-            if(kullanicilar[0]["eposta"] === req.body.eposta){
-              if(kullanicilar[0]["sifre"] === req.body.sifre ){
-                res.send(kullanicilar)
-                next();
-              }else{
-                res.send("sifre yanlis")
-                next();
-              }
-              
+            if(kullanicilar[0]["sifre"] === req.body.sifre ){
+              res.send(kullanicilar)
+              next();
           }
           else{
             res.send("eposta veya sifre yanlis")
