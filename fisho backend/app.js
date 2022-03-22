ebb4fd1791d088be2a75adc411c7be61f309be92
mertkorayates <mertkorayates200@gmail.com>
2022-03-22 21:39:05 +0300

    asd
    asd

--- fisho backend/app.js
+++ fisho backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({
@@ -8,6 +12,12 @@ app.get('/', (req, res) => {
       yas:"22"
   })
 })
+
+
+app.post('/gonder', (req, res) => {
+    const { name, surName } = req.body;
+    res.send(`Name : ${name} - Surname : ${surName}`);
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
