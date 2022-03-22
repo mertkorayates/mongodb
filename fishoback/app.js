a8d45f16813d45937244769b36c35b3e3eb7a209
mertkorayates <mertkorayates200@gmail.com>
2022-03-22 22:28:48 +0300

    bb
    nn

---
+++ fishoback/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({
+      isim: "mert",
+      yas:"22"
+  })
+})
+
+
+app.post('/gonder', (req, res) => {
+    const { name, surName } = req.body;
+    res.send(`Name : ${name} - Surname : ${surName}`);
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
