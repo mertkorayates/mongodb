a2a88e1da943e93def01bcf70d8558068c3b0cab
mertkorayates <mertkorayates200@gmail.com>
2022-05-01 21:34:41 +0300

    Update index.js

--- socketiotest/index.js
+++ socketiotest/index.js
@@ -10,15 +10,16 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
-mongoose.connect('', {
+mongoose.connect('mongodb://127.0.0.1:27017/fisho', {
   useNewUrlParser: true
-}).then((gek)=>console.log(gek))
+})
 
 const Schema = mongoose.Schema;
 let devices = new Schema({
   mac: String,
   adjTemps: String,
   manuelHeart: String,
+  bleUuid: String,
   socketId: String,
   temps: [String],
 
@@ -32,21 +33,21 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-      socket.emit("deneme","naber")
+      //socket.emit("deneme","naber")
        socket.on("writeTemp",(msg)=>{
         console.log(msg)
         
           
-       /*
+       
 
-device.findOneAndUpdate({mac:msg.mac},{adjTemps:msg.adjTemps,socketId:socket.id},(err,dat)=>{
+device.findOneAndUpdate({mac:msg.bleUuid},{adjTemps:msg.adjTemps,socketId:socket.id},(err,dat)=>{
          if(err){
            console.log(err)
          }
          console.log(dat)
        })
 
-       */
+       
 
 
 
@@ -54,7 +55,7 @@ device.findOneAndUpdate({mac:msg.mac},{adjTemps:msg.adjTemps,socketId:socket.id}
 
       socket.on("onHeart",(msg)=>{
 
-        device.findOneAndUpdate({mac:msg.mac},{manuelHeart:"11",socketId:socket.id},(err,dat)=>{
+        device.findOneAndUpdate({mac:msg.bleUuid},{manuelHeart:"11",socketId:socket.id},(err,dat)=>{
           if(err){
             console.log(err)
           }
@@ -67,7 +68,7 @@ device.findOneAndUpdate({mac:msg.mac},{adjTemps:msg.adjTemps,socketId:socket.id}
 
        socket.on("offHeart",(msg)=>{
 
-        device.findOneAndUpdate({mac:msg.mac},{manuelHeart:"00",socketId:socket.id},(err,dat)=>{
+        device.findOneAndUpdate({mac:msg.bleUuid},{manuelHeart:"00",socketId:socket.id},(err,dat)=>{
           if(err){
             console.log(err)
           }
@@ -105,9 +106,9 @@ app.post("/datapush",(req,res,next)=>{
       device.find({mac:req.body.MAC},(err,result)=>{
         if(result.length >= 1){
      
-          device.findOneAndUpdate({mac: result[0].mac},{$push:{temps:req.body.TEMP}},(err,resx)=>{
-            io.emit(resx["mac"],{temps:resx["temps"]})
-            io.emit(`${resx["mac"]} realtime`,{temps:req.body.TEMP})
+          device.findOneAndUpdate({mac: result[0].mac},{$push:{temps:req.body.TEMP},bleUuid:req.body.UUID},(err,resx)=>{
+            io.emit(resx["bleUuid"],{temps:resx["temps"]})
+            io.emit(`${resx["bleUuid"]} realtime`,{temps:req.body.TEMP})
             console.log(resx)
             res.send({"adjTemps":resx["adjTemps"],"manuelHeart":resx["manuelHeart"]})
           })
@@ -115,7 +116,7 @@ app.post("/datapush",(req,res,next)=>{
          console.log(result[0].mac)
         } 
         else{
-          device.create({mac:req.body.MAC,adjTemps:"",manuelHeart:"",socketId:"",temps:[req.body.TEMP]},(err,resx)=>{
+          device.create({mac:req.body.MAC,adjTemps:"",manuelHeart:"",socketId:"",bleUuid:"",temps:[req.body.TEMP]},(err,resx)=>{
             console.log("YOKTU OLUSTURULDU")
             res.send({"adjTemps":resx["adjTemps"],"manuelHeart":resx["manuelHeart"]})
           })
